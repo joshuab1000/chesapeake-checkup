6d8e69ecc5ed3a1d9f6766d504186231489f247e
Joshua B <joshuab5@umbc.edu>
2023-04-11 01:48:50 -0400

    Add function to parse the latest data from selected location

--- TODO.md
+++ TODO.md
@@ -5,12 +5,17 @@
 - Allow user to enter text in location select combobox. Suggest autofill options for it, and return to the deafult value if they delete their entry.
 - Create a logo and set it as the icon.
 - Create classes for each window.
+- Add graph on stats window.
+- Add descriptions of each parameter as popups.
+- Prevent a new window from opening if the last window that was opened has the same location name.
 
 ### In Progress ···
 
-- Access variable from location combobox when select button is pressed.
-- Open new window when select button is pressed.
+- Create a loop that adds these as elements instead of printing them in the console.
+- Then, write a function that gets the full name and description of each parameter.
 
 ### Done ✓
 
 - Populate location select combobox with locations from the API.
+- Access variable from location combobox when select button is pressed.
+- Open new window when select button is pressed.
--- home.py
+++ home.py
@@ -29,7 +29,8 @@ LABEL_FONT = (FONT, LABLE_SIZE)
 
 root = tk.Tk()
 # Location selected from main window combobox
-selected_location = tk.StringVar()
+selected_location_name = tk.StringVar()
+# available_locations = []
 
 def get_locations():
     """Return a dictionary of all available locations with ID and Name attributes."""
@@ -55,7 +56,29 @@ def get_location_names(available_locations):
         location_names.append(location["name"])
     return location_names
 
-def get_data():
+def get_location_id(location_name):
+    """Get the ID of a location by its name"""
+    available_locations = get_locations()
+    for location in available_locations:
+        if location["name"] == location_name:
+            return location["id"]
+
+def get_latest_data(water_quality_data):
+    """Get the most recently uploaded data, return it as a list of dictionaries with paramaters as the keys"""
+    latest_data = {}
+    
+    for sample in water_quality_data:
+        # current_data = {"Parameter":sample["Parameter"], "MeasureValue":sample["MeasureValue"], "SampleDate":sample["SampleDate"]}
+        parameter = sample["Parameter"]
+        if parameter not in latest_data.keys():
+            latest_data[parameter] = sample
+        else:
+            if sample["SampleDate"] > latest_data[parameter]["SampleDate"]:
+                latest_data[parameter] = sample
+    return(latest_data)
+
+def get_parameter_info(parameter):
+    """Get name and definition of each parameter"""
     pass
 
 def home_window():
@@ -76,7 +99,7 @@ def home_window():
     select_label = tk.Label(select_location_frame, text="Select a Watershed:", font=BUTTON_FONT)
     select_label.pack(side=tk.TOP, anchor=tk.NW)
     
-    location_cb = ttk.Combobox(select_location_frame, textvariable=selected_location, state="readonly", font=BUTTON_FONT)
+    location_cb = ttk.Combobox(select_location_frame, textvariable=selected_location_name, state="readonly", font=BUTTON_FONT)
 
     # Populate combobox with locations sorted by name
     available_locations = get_locations()
@@ -85,6 +108,10 @@ def home_window():
     location_cb["values"] = location_names
     location_cb.set("Select a Watershed")
     location_cb.pack(side=tk.LEFT, padx=5)
+
+    # print(available_locations[selected_location_name]["id"])
+
+    # available_locations[selected_location_name]
 
     """
         Create a function to read the combobox every time it is edited and suggest autofill options as the user types.
@@ -100,44 +127,35 @@ def home_window():
 
 def stats_window():
     stats_window = tk.Toplevel(root)
-    location = selected_location.get()
+    location_name = selected_location_name.get()
+    
     stats_window.geometry("500x600")
-    stats_window.title(location)
+    stats_window.title(location_name)
     # Create a Label in New window
-    title = tk.Label(stats_window, text=location, font=TITLE_FONT)
+    title = tk.Label(stats_window, text=location_name, font=TITLE_FONT)
     title.pack(pady=10)
+    
+    location_id = str(get_location_id(location_name))
     
     recent_measurements_frame = tk.Frame(stats_window)
     measurements_label = tk.Label(recent_measurements_frame, text="Recent Measurements:", font=LABEL_FONT)
 
     measurements_label.pack()
-    url = 'https://datahub.chesapeakebay.net/api.JSON/' + section + '/' + subsection +'/' + start_date + '/' + end_date + '/' + data_stream_data + '/' + program_id + '/' + project_id + '/' + geographical_attribute +'/' + attribute_id + '/' + substance_ids
-    # print(url)
-    water_quality = requests.get(url)
     
+    url = 'https://datahub.chesapeakebay.net/api.JSON/' + section + '/' + subsection +'/' + start_date + '/' + end_date + '/' + data_stream_data + '/' + program_id + '/' + project_id + '/' + geographical_attribute +'/' + location_id + '/' + substance_ids
+
+    water_quality = requests.get(url)
     water_quality_data = json.loads(water_quality.text)
     
-    latest_data = {}
+    latest_data = get_latest_data(water_quality_data)
     
-    for sample in water_quality_data:
-        # current_data = {"Parameter":sample["Parameter"], "MeasureValue":sample["MeasureValue"], "SampleDate":sample["SampleDate"]}
-        parameter = sample["Parameter"]
-        if parameter not in latest_data.keys():
-            latest_data[parameter] = sample
-        else:
-            if sample["SampleDate"] > latest_data[parameter]["SampleDate"]:
-                latest_data[parameter] = sample
-    print(latest_data)
-        # for data in latest_data:
-        #     if (data["Parameter"] == current_data["Parameter"]) and (data["SampleDate"] > current_data["SampleDate"]):
-        #         latest_data.append(current_data)
-                
-        # if (relavent_data["Parameter"] not in latest_data):
-        #     latest_data.append(relavent_data)
-    # print(water_quality_data)
-    # print(latest_data)
-
-    
+    for key in latest_data:
+        print(key + ':', latest_data[key]["MeasureValue"], latest_data[key]["Unit"], '(' + latest_data[key]["SampleDate"] + ')')
+        
+    """
+    Create a loop that adds these as elements instead of printing them in the console.
+    Then, write a function that gets the full name and description of each parameter.
+    """
     
     recent_measurements_frame.pack(side=tk.LEFT, anchor=tk.NW, padx=20)
     
